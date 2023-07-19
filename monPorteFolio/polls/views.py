06ac235135aeff7a295208b75c5707f17d5a5c65
thomas <thomas.rud@outlook.fr>
2023-07-19 16:25:03 +0200

    avancé 19/07/2023 16:25

--- monPorteFolio/polls/views.py
+++ monPorteFolio/polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
     text="""<h1>Mon CV </h1> 
     <br> 
     <p>Qui suis je ? </p>"""
     return HttpResponse(text)
+
+def date_actuelle(request):
+    return render(request,'polls/date.html', {'date':datetime.now()})
